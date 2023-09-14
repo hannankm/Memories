8a446745bc7f3b274774b854cb47596572920166
Hanan kedir <hanankmoh@gmail.com>
2023-09-14 23:57:37 +0300

    Controller, Route, and Model setup

--- server/index.js
+++ server/index.js
@@ -3,4 +3,29 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
 const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://hannankm:275as123@cluster0.h19idjd.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.eventNames.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    })
+  )
+  .catch((error) => console.log(error.message));
+
+//mongo db cloud atlas version - host out db on their cloud
